ccb440f80ac173889616c9bab4b376e7ffe83331
Huzaifa <huzaifa593@hotmail.com>
2024-05-27 04:21:17 -0400

    update

---
+++ scripts/start-qemu.sh
@@ -0,0 +1,10 @@
+qemu-system-x86_64 \
+	-enable-kvm \
+	-m 8000 \
+	-kernel $1 \
+	-drive file=$2,if=virtio,format=raw \
+	-append "rootwait root=/dev/vda console=tty1 console=ttyS0" \
+	-serial mon:stdio \
+	-net nic,model=virtio \
+	-net user \
+	-nographic
