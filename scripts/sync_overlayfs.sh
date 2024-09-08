56a069b4266d55080d543e1651d5baf98cbb390e
Huzaifa <huzaifa593@hotmail.com>
2024-09-07 20:02:50 -0400

    dont use ubuntu. use python3 in dockerfile

--- scripts/sync_overlayfs.sh
+++ scripts/sync_overlayfs.sh
@@ -17,6 +17,16 @@ files=(
     "../filesystem/target/usr/bin/runc"
 )
 
+# Loop through each file and remove it
+for file in "${files[@]}"; do
+    if [ -f "$file" ]; then  # Check if file exists before attempting to remove
+        rm -f "$file"
+        echo "Removed: $file"
+    else
+        echo "File does not exist: $file"
+    fi
+done
+
 
 # Synchronize overlayfs/houdini with target/houdini
 rsync -av --delete "$overlayfs_dir" "$target_dir"
